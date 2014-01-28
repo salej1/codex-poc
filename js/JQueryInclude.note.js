c6152d1741b537f0e1f8077e8bab8e47f03cb59f
salej1 <salej1@hotmail.com>
2014-01-28 02:30:54 -0600

    Commit Inicial

---
+++ js/JQueryInclude.note.js
@@ -0,0 +1,21 @@
+		<link rel="stylesheet" href="css/jquery.ui.theme.css">
+		<link rel="stylesheet" href="css/jquery-ui.min.css">
+		<script src="js/jquery-1.10.1.min.js"></script>
+		<script src="js/jquery-ui.js"></script>
+		<script src="js/jquery.ui.touch-punch.min.js"></script>
+		<script src="js/jquery.ui.core.js"></script>
+		<script src="js/jquery.ui.widget.js"></script>
+		<script src="js/jquery.ui.mouse.js"></script>
+		<script src="js/jquery.ui.button.js"></script>
+		<script src="js/jquery.ui.draggable.js"></script>
+		<script src="js/jquery.ui.position.js"></script>
+		<script src="js/jquery.ui.resizable.js"></script>
+		<script src="js/jquery.ui.button.js"></script>
+		<script src="js/jquery.ui.dialog.js"></script>
+		<script src="js/jquery.ui.effect.js"></script>
+		<script src="js/jquery.signature.min.js"></script>
+		<script type="text/javascript">
+			$(function(){
+				// INITIALIZATION SCRIPTS HERE
+			});
+		</script>
